40488bc26b07a28eddc67f49a01dbdc97ca9327a
Andrew Leach <leach.andrew@gmail.com>
2021-05-13 22:20:18 -0600

    new version

--- st_3.R
+++ st_3.R
@@ -20,129 +20,122 @@ keep_rows<-c("Crude Oil Light",  "Crude Oil Medium",
              "Non-Upgraded Total","Upgraded Production","Total Oil Sands Production",
              "Total Production")
 
+#use a list to store each of the data sets we're going to download
 data_store <- list()
 years<-seq(2010,2016)
 i<-1
-for(year in years){
-  #year<-2016
+for(year in years){ #loop over 2010-2016, but the data are stored in different sheets
   filename<-paste("Oil_2010-2016.xlsx",sep="")
-  production_data <- read_xlsx(filename, sheet = as.character(year), skip = 4)
+  if(tolower(download)=="all")#if you chose to download the old data. correct case errors in case someone sends "ALL"
+    if(has_internet()) #if you're connected, go get the file
+      download.file(paste("https://www.aer.ca/documents/sts/st3/",filename,sep=""),filename,mode="wb")
+  production_data <- read_excel(filename, sheet = as.character(year), skip = 4)
+  #the sheets are a bit of a mess, so we need to clean them up
+  names(production_data)[1]<-"product" #name column 1
+  # the %>% or pipeline is basically a  "pass to" command
+  # start with production data, pass to select and remove cols 2 and 15, pass to filter and keep and rows with
+  # the names in the keep_rows object we created above
+  production_data<-production_data %>% select(-2,-15) %>% filter(product %in% keep_rows)
+  #rename the column with the annual data s (grep is a search function that, in this case, is looking for
+  #whatever year we're processing and it will name the column annual
+  names(production_data)[grep(as.character(year),names(production_data))]<-"annual"
+  #assign the year to whatver year we're processing
+  production_data$year<-year
+  # create a long-form data set that will have product, annual level for that product, the year we're processing, and     #the monthly data
+  production_data<-production_data %>% pivot_longer(cols=-c("product","annual","year"),
+                                                    names_to="month",values_to = "production")
+  #store those data in a node in the list
+  data_store[[i]]<-production_data
+  #step your list counter one unit
+  i<-i+1
+}
+#let's get 2017-2020 data - same process, but they are in seperate files
+years<-seq(2017,2020)
+for(year in years){ #loop over 2017-2020
+  filename<-paste("Oil_",year,".xlsx",sep="") #use paste to put the year into the filename
+  if(tolower(download)=="all")#if you chose to download the old data. correct case errors in case someone sends "ALL"
+    if(has_internet()) #if you're connected, go get the file
+      download.file(paste("https://www.aer.ca/documents/sts/st3/",filename,sep=""),filename,mode="wb")
+  production_data <- read_excel(filename, sheet = "Data", skip = 4)
   names(production_data)[1]<-"product"
   production_data<-production_data %>% select(-2,-15) %>% filter(product %in% keep_rows)
   names(production_data)[grep(as.character(year),names(production_data))]<-"annual"
   production_data$year<-year
-  production_data<-production_data %>% melt(id=c("product","annual","year"),
-                                            variable.name="month",value.name = "production")
+  production_data<-production_data %>% pivot_longer(cols=-c("product","annual","year"),
+                                                    names_to="month",values_to = "production")
   data_store[[i]]<-production_data
   i<-i+1
-  }
-  year<-2017
-  filename<-paste("Oil_2017.xlsx",sep="")
-  production_data <- read_xlsx(filename, sheet = "Data", skip = 4)
-  names(production_data)[1]<-"product"
-  production_data<-production_data %>% select(-2,-15) %>% filter(product %in% keep_rows)
-  names(production_data)[grep(as.character(year),names(production_data))]<-"annual"
-  production_data$year<-year
-  production_data<-production_data %>% melt(id=c("product","annual","year"),
-                                          variable.name="month",value.name = "production")
-  data_store[[i]]<-production_data
-  i<-i+1
-  year<-2018
-  filename<-paste("Oil_2018.xlsx",sep="")
-  production_data <- read_xlsx(filename, sheet = "Data", skip = 4)
-  names(production_data)[1]<-"product"
-  production_data<-production_data %>% select(-2,-15) %>% filter(product %in% keep_rows)
-  names(production_data)[grep(as.character(year),names(production_data))]<-"annual"
-  production_data$year<-year
-  production_data<-production_data %>% melt(id=c("product","annual","year"),
-                                            variable.name="month",value.name = "production")
-  
-  
-  data_store[[i]]<-production_data
-  
-  i<-i+1
-  year<-2019
-  filename<-paste("Oil_2019.xlsx",sep="")
-  production_data <- read_xlsx(filename, sheet = "Data", skip = 4)
-  names(production_data)[1]<-"product"
-  production_data<-production_data %>% select(-2,-15) %>% filter(product %in% keep_rows)
-  names(production_data)[grep(as.character(year),names(production_data))]<-"annual"
-  production_data$year<-year
-  production_data<-production_data %>% melt(id=c("product","annual","year"),
-                                            variable.name="month",value.name = "production")
-  
-  data_store[[i]]<-production_data
-  
-  i<-i+1
-  year<-2020
-  filename<-paste("Oil_2020.xlsx",sep="")
-  #download.file("https://www.aer.ca/documents/sts/st3/Oil_2020.xlsx",filename,mode="wb")
-  production_data <- read_xlsx(filename, sheet = "Data", skip = 4)
-  names(production_data)[1]<-"product"
-  production_data<-production_data %>% select(-2,-15) %>% filter(product %in% keep_rows)
-  names(production_data)[grep(as.character(year),names(production_data))]<-"annual"
-  production_data$year<-year
-  production_data<-production_data %>% melt(id=c("product","annual","year"),
-                                            variable.name="month",value.name = "production")
-  
-  data_store[[i]]<-production_data
-  
-  
-  i<-i+1
-  year<-"Current"
-  filename<-paste("Oil_current.xlsx",sep="")
-  #check for internet
-  if(has_internet()){
-    download.file("https://www.aer.ca/documents/sts/st3/Oil_current.xlsx",filename,mode="wb")
-  }
-  
-  production_data <- read_xlsx(filename, sheet = "Data", skip = 4)
-  year<-2021
-  names(production_data)[1]<-"product"
-  production_data<-production_data %>% select(-2,-15) %>% filter(product %in% keep_rows)
-  names(production_data)[grep(as.character(year),names(production_data))]<-"annual"
-  production_data$year<-year
-  production_data<-production_data %>% melt(id=c("product","annual","year"),
-                                            variable.name="month",value.name = "production")
-  production_data<-production_data %>% filter(production!=0)
-  data_store[[i]]<-production_data
-  
-  all_production<-do.call(rbind,data_store)
-  all_production<-all_production %>% mutate(production=as.numeric(production),
-                                            date=ymd(paste(year,as.character(month),1,sep="-")))
-         
+}
+#Most recent data are stored as "current"  
+year<-"Current"
+filename<-paste("Oil_current.xlsx",sep="")
+#check for internet
+if(tolower(download)%in% c("all","current"))#if you chose to download all or current data. correct case errors
+  if(has_internet()) #if you're connected, go get the file
+    download.file(paste("https://www.aer.ca/documents/sts/st3/",filename,sep=""),filename,mode="wb")
+production_data <- read_excel(filename, sheet = "Data", skip = 4)
+year<-2021
+names(production_data)[1]<-"product"
+production_data<-production_data %>% select(-2,-15) %>% filter(product %in% keep_rows)
+names(production_data)[grep(as.character(year),names(production_data))]<-"annual"
+production_data$year<-year
+production_data<-production_data %>% pivot_longer(cols=-c("product","annual","year"),
+                                                  names_to="month",values_to = "production")
+production_data<-production_data %>% filter(production!=0)#keep only non-zero production
+data_store[[i]]<-production_data
 
-  graph_data<-c("Conventional Light Oil Production","Conventional Heavy Oil Production","Condensate Production",
-               "In Situ Bitumen Production","Mined Bitumen Production")
-  
-  all_production <-all_production %>% mutate(product=as_factor(product)) %>%mutate(
-                                             product=fct_recode(product,
-                                                                "In Situ Bitumen Production"="In Situ Production",
-                                                                "Mined Bitumen Production"="Mined Production"
-                                                                ),
-                                             product=fct_relevel(product,"Mined Bitumen Production",after=5))
+#now, stack all the elements stored in your list of data into a data frame
+all_production<-do.call(rbind,data_store)
+#now, we'll manipulate the data. mutate is adding a column based on a calculation
+#so, all_production is all_prodcution passed to mutate, where the production variable is set to numeric
+#and we format the dates using ymd since that's the format in the spreadsheet
+# (remember, the lubridate package makes dates easy)
+all_production<-all_production %>% mutate(production=as.numeric(production),
+                                          date=ymd(paste(year,as.character(month),1,sep="-")))
+
+
+#next part is working with factors. Factors basically store data as a numeric code or level (1,2,3,4) and a
+#set of labels attached to the levels
+
+#take all_production,and pass to mutate, change the column product to a factor 
+all_production <-all_production %>% mutate(product=as_factor(product)) %>%
+  #and now we're going to re-code some of the labels so that we make them common                                          #across the data we loaded
+  mutate(
+    product=fct_recode(product,
+                       "In Situ Bitumen Production"="In Situ Production",
+                       "Mined Bitumen Production"="Mined Production"
+    ),
+    #and we're going to sort them so that mined bitumen is last
+    product=fct_relevel(product,"Mined Bitumen Production",after=5))
+#last, we're going to collapse factors into on common level, so light and medium go into light, heavy and ultra-heavy 
+#are classified as heavy
+
+all_production <-all_production %>% mutate(product=fct_collapse(product,
+                                                                "Conventional Light Oil Production" = c("Crude Oil Light","Crude Oil Medium"),
+                                                                "Conventional Heavy Oil Production"= c("Crude Oil Heavy","Crude Oil Ultra Heavy")
+)) %>% 
+  #now, because for some rows we'll now have 2 entries for production from conventional light oil, for example, 
+  # in a month, we have to collapse them
+  #group your data by product, year, month, and date
+  group_by(product,year,month,date)%>%
+  #and combined the monthly and annual production from all prducts with the same label by summing them
+  summarize(production=sum(production),annual=sum(as.numeric(annual))) %>%
+  ungroup()
+
+#last thing - we'll use zoo to create some time series descriptives
+#add year and quarter
+all_production <-all_production %>% mutate(year=year(date), quarter=quarter(date))%>%
+  #add rolling quarterly and annual data and lags
+  group_by(product)%>% arrange(date)%>% mutate(
+    roll_4m=roll_mean(production,4),
+    roll_12m=roll_sum(production,12),
+    growth_12m=(roll_12m-lag(roll_12m,12))/lag(roll_12m,12),
+    lag12_raw=(production-lag(production,12))/lag(production,12),
+    lag12_roll=(roll_4m-lag(roll_4m,12))/lag(roll_4m,12))
+#and that's it - you've made a data set of Alberta oil production for the last decade
 
   
-  
-   
-  all_production <-all_production %>% mutate(product=fct_collapse(product,
-                        "Conventional Light Oil Production" = c("Crude Oil Light","Crude Oil Medium"),
-                        "Conventional Heavy Oil Production"= c("Crude Oil Heavy","Crude Oil Ultra Heavy")
-                        )) %>% 
-                    group_by(product,year,month,date)%>%
-                          summarize(production=sum(production),annual=sum(as.numeric(annual))) %>%
-                          ungroup()
-    
-  
-  all_production <-all_production %>% mutate(year=year(date), quarter=quarter(date))%>%
-    group_by(product)%>% arrange(date)%>% mutate(
-      roll_4m=roll_mean(production,4),
-      roll_12m=roll_sum(production,12),
-      growth_12m=(roll_12m-lag(roll_12m,12))/lag(roll_12m,12),
-      lag12_raw=(production-lag(production,12))/lag(production,12),
-      lag12_roll=(roll_4m-lag(roll_4m,12))/lag(roll_4m,12))
-  
-  
+
   annual<-all_production %>% group_by(year,product) %>% summarise(production=sum(production))
   annual <-annual %>%arrange(year)%>% group_by(product)%>%
     mutate(yoy_growth=round((production-lag(production,1))/lag(production,1)*100,2))
@@ -152,7 +145,13 @@ for(year in years){
   quarterly<-all_production %>% group_by(year,quarter,product) %>% summarise(production=mean(production))
   quarterly <-quarterly %>%arrange(year,quarter)%>% group_by(product)%>%
     mutate(q_lasty=round((production-lag(production,4))/lag(production,4)*100,2))
+
   
+    graph_data<-c("Conventional Light Oil Production","Conventional Heavy Oil Production","Condensate Production",
+              "In Situ Bitumen Production","Mined Bitumen Production")
+  
+  
+    
 ggplot(filter(all_production,product%in%graph_data))+
   geom_area(aes(date,m3_bbl(production)/days_in_month(date)/10^6,group=product,fill=product))+
   scale_fill_manual("",values = colors_tableau10(),guide = "legend")+
